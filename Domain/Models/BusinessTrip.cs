cadbd24dbfcd043f47f2620579bb2acd5cd87b53
Oleh Shemeliak <shemeliak.oleh.main@gmail.com>
2022-12-09 14:49:30 +0200

    Added ApplicationProject

--- Domain/Models/BusinessTrip.cs
+++ Domain/Models/BusinessTrip.cs
@@ -1,7 +1,5 @@
 namespace Domain.Models
 {
-    using System;
-    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
 
--- UPA_MVC/Controllers/AccountController.cs
+++ UPA_MVC/Controllers/AccountController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using UPA_MVC.Models;
 
 namespace UPA_MVC.Controllers;
 public class AccountController : Controller
@@ -28,6 +27,6 @@ public class AccountController : Controller
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View();
     }
 }
